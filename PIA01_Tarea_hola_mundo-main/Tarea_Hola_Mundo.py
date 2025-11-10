88aea598758ec43ec5fd5b78c26e773b0e6e2889
manuruizflor870 <m.ruizflores.ed05@gnossuam.es>
2025-11-10 20:35:15 +0100

    Delete Tarea_Hola_Mundo.py

--- PIA01_Tarea_hola_mundo-main/Tarea_Hola_Mundo.py
+++
@@ -1,5 +0,0 @@
-### Archivo `hola_mundo.py` (Lenguaje de Programación Python). Este archivo contiene el código más básico.
-# Nombre Archivo: hola_mundo.py
-# Python es un lenguaje de alto nivel cuyo objetivo principal es la legibilidad.
-# Una de sus principales ventajas es la facilidad para programar tareas sencillas.
-print("Hola Mundo")
