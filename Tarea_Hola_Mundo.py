d2c378bc004a0f48f635dc813bdb7d6590811813
manuruizflor870 <m.ruizflores.ed05@gnossuam.es>
2025-11-10 17:35:34 +0100

    Add comentarios y código Tarea_Hola_Mundo.py
    Added comments explaining the purpose of the file and Python.

--- Tarea_Hola_Mundo.py
+++ Tarea_Hola_Mundo.py
@@ -1,1 +1,6 @@
-
+### Archivo `hola_mundo.py` (Lenguaje de Programación Python). Este archivo contiene el código más básico.
+```Python
+# Archivo: hola_mundo.py
+# Python es un lenguaje de alto nivel cuyo objetivo principal es la legibilidad.
+# Una de sus principales ventajas es la facilidad para programar tareas sencillas.
+print("Hola Mundo")
